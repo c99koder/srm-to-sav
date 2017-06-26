3c6267d35e9aaeb577132b3a3b8b2f5eafc92a6b
Sam Steele <sam.steele@gmail.com>
2017-06-26 07:24:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # srm-to-sav
 Convert RetroArch SRM files to raw GBA SAV files and vice versa
+
+# Usage
+
+```
+sav-to-srm.py -i <input.sav> -o <output.srm> [--byteswap]
+```
+
+# Byte Swapping
+
+Some games, such as The Legend of Zelda - The Minish Cap write their save data backwards in RetroArch.  Use the `--byteswap` argument to write the data in the correct order.
---
+++ srm-to-sav.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def usage():
+	print 'Usage: srm-to-sav.py -i <input.srm> -o <output.sav> [--byteswap]'
+	print 'Converts RetroArch SRM files to raw GBA SAV format'
+
+def main(argv):
+	inputfile = None
+	outputfile = None
+	byteswap = None
+
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:",["input=","output=","byteswap"])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+		
+	for opt, arg in opts:
+		if opt == '-h':
+			usage()
+			sys.exit()
+		elif opt in ("-i", "--input"):
+			inputfile = arg
+		elif opt in ("-o", "--output"):
+			outputfile = arg
+		elif opt == "--byteswap":
+			byteswap = True
+			
+	if (not inputfile or not outputfile):
+		usage()
+		sys.exit(2)
+
+	with open(inputfile, "rb") as infile, open(outputfile, "wb") as outfile:
+		byte = infile.read(1)
+		while ord(byte) == 255:
+			byte = infile.read(1)
+		
+		data = byte + infile.read()
+		
+		if byteswap:
+			for i in xrange(len(data) / 8):
+				outfile.write(data[i*8+7])
+				outfile.write(data[i*8+6])
+				outfile.write(data[i*8+5])
+				outfile.write(data[i*8+4])
+				outfile.write(data[i*8+3])
+				outfile.write(data[i*8+2])
+				outfile.write(data[i*8+1])
+				outfile.write(data[i*8])
+		else:
+			outfile.write(data)
+			
+		print "Extracted", len(data), "bytes from", inputfile, "to", outputfile
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
